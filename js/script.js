62bb3e92b0826eed340782625752430300943fa9
Hamish Gray <hamishgray@Hamishs-MacBook-Pro.local>
2017-04-18 15:39:49 +0100

    initial setup
    Initial setup adding latest styling and structure from other microsites
    to the template

---
+++ js/script.js
@@ -0,0 +1,135 @@
+(function ($, root, undefined) {$(function () {'use strict'; // on ready start
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////
+//        general
+///////////////////////////////////////
+
+  // css tricks snippet - http://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
+
+  // inserts current year
+  $('.js-year').html(new Date().getFullYear());
+
+  // detects touch device
+  if ("ontouchstart" in document.documentElement){
+    $('html').addClass('touch');
+  }
+
+
+///////////////////////////////////////
+//        Navigation
+///////////////////////////////////////
+
+  // mobile nav toggle open & close
+  $('.js-toggle-mobile-nav').on('click', function(e) {
+    $('.mobile-nav').toggleClass('is-open').toggleClass('is-closed');
+  });
+
+  // current page nav highlight
+  var currentPage = $('body').data('current-page');
+  $('.' + currentPage + ' .site-nav__item--' + currentPage).addClass('site-nav__item--current');
+
+
+///////////////////////////////////////
+//      SVG image swap
+///////////////////////////////////////
+
+  // finds image with class and swaps .png with .svg in img source string
+  if (Modernizr.svgasimg) {
+    var svgSwap = $('img.js-svg-swap');
+    svgSwap.each( function() {
+      var currentSrc = $(this).attr('src'),
+          newSrc = currentSrc.replace('.png', '.svg');
+      $(this).attr('src', newSrc);
+    });
+  }
+
+
+///////////////////////////////////////
+//      Parallax
+//      [ example: <div class="parallax" data-parallax-speed="0.2"> ]
+///////////////////////////////////////
+
+  // $(document).scroll(function(){
+  //   var scrolled = $(document).scrollTop();
+  //   $('.parallax').each(function(){
+  //     var speed = $(this).attr('data-parallax-speed');
+  //     var offset = $(this).offset();
+  //     var parallax = -(scrolled - offset.top) * speed ;
+  //     $(this).css('background-position', 'center ' + parallax + 'px');
+  //   });
+  // });
+
+
+///////////////////////////////////////
+//    Generic modal
+///////////////////////////////////////
+
+  var modal          = $('.js-modal'),
+      modalLaunchBtn = $('.js-open-modal'),
+      modalCloseBtn  = $('.js-close-modal');
+
+    // opens modal
+    function modalOpen(event){
+      event.preventDefault();
+      // disable scrolling on background content (doesn't work iOS)
+      $('body').addClass('disable-scroll');
+      // // open modal
+      modal.fadeIn('250', function(){
+        $(this).removeClass('is-closed').addClass('is-open');
+      });
+    }
+
+    // closes modal
+    function modalClose(event){
+      event.preventDefault();
+      // enable scrolling
+      $('body').removeClass('disable-scroll');
+      // close modal with fade
+      modal.fadeOut('250', function(){
+        $(this).removeClass('is-open').addClass('is-closed');
+      });
+    }
+
+    // launches modal when offer is clicked
+    modalLaunchBtn.on('click', function(event) {
+      modalOpen(event);
+    });
+
+    // closes modal on close icon click
+    modalCloseBtn.on('click', function(event) {
+      modalClose(event);
+    });
+
+    // closes modal on background click
+    modal.on('click', function(event) {
+      if (event.target !== this){
+        return;
+      }
+      modalClose(event);
+    });
+
+    // closes modal on escape key press
+    $(document).keyup(function(event) {
+       if (event.keyCode == 27) {
+         modalClose(event);
+        }
+    });
+
+
+///////////////////////////////////////////////////////////////////////////////
+});})(jQuery, this); // on ready end
